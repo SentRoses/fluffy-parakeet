8bcb2457c6ed322c8bd1deed7623f6aca64355b0
tr-adubois <tr.adubois@gmail.com>
2018-02-08 12:06:02 +0100

    Edits to code

--- src/app/providers/pizza.provider.ts
+++ src/app/providers/pizza.provider.ts
@@ -6,10 +6,10 @@ import { Pizza } from '../model/pizza';
 export class PizzaProvider {
     getPizzas() {
         return Promise.resolve([
-            { id: 1, name: 'Reine', price: 12, img: "../assets/img/pizza-reine.png" },
-            { id: 2, name: '4 fromages', price: 13, img: "../assets/img/pizza-4fromages.png" },
-            { id: 3, name: 'Orientale', price: 11, img: "../assets/img/pizza-orientale.png" },
-            { id: 4, name: 'Cannibale', price: 9, img: "../assets/img/pizza-cannibale.png" }
+            { id: 1, name: 'Reine', price: 12, img: "assets/img/pizza-reine.png" },
+            { id: 2, name: '4 fromages', price: 13, img: "assets/img/pizza-4fromages.png" },
+            { id: 3, name: 'Orientale', price: 11, img: "assets/img/pizza-orientale.png" },
+            { id: 4, name: 'Cannibale', price: 9, img: "assets/img/pizza-cannibale.png" }
         ]);
     }
     getPizzasSlowly(): Promise<Pizza[]> {
